3333b3f5468b2f8ee8bd933006be0cdadb15a77f
rakshith <rakshithhemanth777@gmail.com>
2023-11-29 11:38:31 +0530

    I am comminting

--- src/main/java/org/example/modules/PayloadManager.java
+++ src/main/java/org/example/modules/PayloadManager.java
@@ -1,4 +1,50 @@
 package org.example.modules;
 
+import io.qameta.allure.internal.shadowed.jackson.core.JsonProcessingException;
+import io.qameta.allure.internal.shadowed.jackson.databind.ObjectMapper;
+import org.example.payloads.Booking;
+import org.example.payloads.Bookingdates;
+import org.example.utils.FakerUtil;
+
 public class PayloadManager {
+    ObjectMapper objectMapper;
+
+    public String createPayload() throws JsonProcessingException {
+        objectMapper = new ObjectMapper();
+
+        Booking booking = new Booking();
+        booking.setFirstname("BS");
+        booking.setLastname("Rakshith");
+        booking.setTotalprice(421);
+        booking.setDepositpaid(true);
+        booking.setAdditionalneeds("lunch");
+
+        Bookingdates bookingdates = new Bookingdates();
+        bookingdates.setCheckin("2023-01-01");
+        bookingdates.setCheckout("2023-01-10");
+        booking.setBookingdates(bookingdates);
+
+        String payload = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(booking);
+        return payload;
+    }
+
+    public String UpdatePayload() throws JsonProcessingException {
+        objectMapper = new ObjectMapper();
+
+        Booking booking = new Booking();
+        booking.setFirstname(FakerUtil.getUserName());
+        booking.setLastname("Rakshith");
+        booking.setTotalprice(421);
+        booking.setDepositpaid(true);
+        booking.setAdditionalneeds("Breakfast, lunch");
+
+        Bookingdates bookingdates = new Bookingdates();
+        bookingdates.setCheckin("2023-01-01");
+        bookingdates.setCheckout("2023-01-10");
+        booking.setBookingdates(bookingdates);
+
+        String payload = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(booking);
+        return payload;
+    }
+
 }
--- src/test/java/org/example/crud/TC_CreateBooking.java
+++ src/test/java/org/example/crud/TC_CreateBooking.java
@@ -1,4 +1,19 @@
 package org.example.crud;
 
+import org.testng.annotations.Test;
+
 public class TC_CreateBooking {
+
+    @Test
+    public void testCreateBooking() {
+        // call the request block
+        // call the payload
+        // Call the asertion block
+    }
+    @Test
+    public void testCreateBooking2(){
+        // call the request block
+        // call the payload
+        // Call the asertion block
+    }
 }
